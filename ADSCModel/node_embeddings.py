c13f10660619d6848eeaf9e2a9579456a5e26eba
ando <ando.cavallari@gmail.com>
2017-02-09 18:19:56 +0800

    cazzo di ubuntu e queue che non vanno

--- ADSCModel/node_embeddings.py
+++ ADSCModel/node_embeddings.py
@@ -6,20 +6,23 @@ import threading
 from queue import Queue
 import numpy as np
 from utils.embedding import chunkize_serial, RepeatCorpusNTimes, train_sg
-
+from itertools import islice, chain, zip_longest
+level = logging.DEBUG
 logger = logging.getLogger('adsc')
+logger.setLevel(level)
 
 
 
 class Node2Vec(object):
-    def __init__(self, workers=1, alpha=0.1, min_alpha=0.0001, negative=0):
+    def __init__(self, workers=1, alpha=0.1, min_alpha=0.0001, negative=0, ):
+
         self.workers = workers
         self.alpha = float(alpha)
         self.min_alpha = min_alpha
         self.negative = negative
         self.window_size = 1
 
-    def train(self, model, edges, _lambda2=0.0, total_node=None, word_count=0, chunksize=500, iter = 1):
+    def train(self, model, edges, _lambda1=1.0, _lambda2=0.0, total_node=None, chunksize=500, iter = 1):
         """
         Update the model's neural weights from a sequence of paths (can be a once-only generator stream).
         """
@@ -31,21 +34,66 @@ class Node2Vec(object):
 
         edges = RepeatCorpusNTimes(edges, iter)
         total_node = edges.total_node
+        logger.info('total edges: %d' % total_node)
+        logger.debug('TEST DEBUG')
+        start, next_report, word_count = time.time(), [5.0], [0]
 
-        start, next_report = time.time(), [1.0]
-        word_count = [word_count]
 
         #int(sum(v.count * v.sample_probability for v in self.vocab.values()))
         jobs = Queue(maxsize=2*self.workers)  # buffer ahead only a limited number of jobs.. this is the reason we can't simply use ThreadPool :(
-        lock = threading.Lock()  # for shared state (=number of words trained so far, log reports...)
+        lock = threading.Lock()
+
+        def prepare_sentences(paths):
+            for path in paths:
+                # avoid calling random_sample() where prob >= 1, to speed things up a little:
+                sampled = [model.vocab[node] for node in path
+                           if node in model.vocab and (model.vocab[node].sample_probability >= 1.0 or model.vocab[node].sample_probability >= np.random.random_sample())]
+                yield sampled
+
+        def batch_generator(iterable, batch_size=1):
+            args = [iterable] * batch_size
+            return zip_longest(*args, fillvalue=None)
+            #     yield chain([batchiter.next()], batchiter)
+
+        # def learn_single(job, word_count):
+        #     py_work = np.zeros(model.layer1_size)
+        #     py_work_o3 = np.zeros(model.layer1_size)
+        #     py_work1_o3 = np.zeros(model.layer1_size)
+        #     py_work2_o3 = np.zeros(model.layer1_size ** 2)
+        #     # update the learning rate before every job
+        #     alpha = max(self.min_alpha, self.alpha * (1 - 1.0 * word_count/ total_node))
+        #     # how many words did we train on? out-of-vocabulary (unknown) words do not count
+        #
+        #
+        #     job_words = sum(train_sg(model.node_embedding, model.node_embedding, path, alpha, self.negative, self.window_size, model.table,
+        #                              py_centroid=model.centroid, py_inv_covariance_mat=model.inv_covariance_mat, py_pi=model.pi, py_k=model.k, py_covariance_mat=model.covariance_mat,
+        #                              py_lambda1=_lambda1, py_lambda2=_lambda2,
+        #                              py_size=model.layer1_size, py_work=py_work, py_work_o3=py_work_o3, py_work1_o3=py_work1_o3, py_work2_o3=py_work2_o3,
+        #                              py_is_node_embedding=1) for path in job if path is not None)
+        #
+        #
+        #
+        #     elapsed = time.time() - start
+        #     if elapsed >= next_report[0]:
+        #         word_count += job_words
+        #         logger.info("PROGRESS: at %.2f%% words, alpha %.05f, %.0f words/s" %
+        #                     (100.0 * word_count / total_node, alpha, word_count / elapsed if elapsed else 0.0))
+        #         next_report[0] = elapsed + 1.0  # don't flood the log, wait at least a second between progress reports
+        #     return word_count
+        # for job_no, job in enumerate(batch_generator(prepare_sentences(edges), chunksize)):
+        #     # logger.debug("putting job #%i in the queue, qsize=%i" % (job_no, jobs.qsize()))
+        #     word_count = learn_single(job, word_count)
 
 
         def worker_train():
             """Train the model, lifting lists of paths from the jobs queue."""
             while True:
-                job = jobs.get()
+                job = jobs.get(block=True)
                 if job is None:  # data finished, exit
+                    logger.debug('thread %s break' % threading.current_thread().name)
+                    jobs.task_done()
                     break
+
 
                 py_work = np.zeros(model.layer1_size)
                 py_work_o3 = np.zeros(model.layer1_size)
@@ -58,45 +106,47 @@ class Node2Vec(object):
 
                 job_words = sum(train_sg(model.node_embedding, model.node_embedding, path, alpha, self.negative, self.window_size, model.table,
                                          py_centroid=model.centroid, py_inv_covariance_mat=model.inv_covariance_mat, py_pi=model.pi, py_k=model.k, py_covariance_mat=model.covariance_mat,
-                                         py_lambda1=1.0, py_lambda2=_lambda2,
+                                         py_lambda1=_lambda1, py_lambda2=_lambda2,
                                          py_size=model.layer1_size, py_work=py_work, py_work_o3=py_work_o3, py_work1_o3=py_work1_o3, py_work2_o3=py_work2_o3,
-                                         py_is_node_embedding=1) for path in job)
-
-
-                with lock:
+                                         py_is_node_embedding=1) for path in job if path is not None)
+                jobs.task_done()
+                lock.acquire(timeout=30)
+                try:
                     word_count[0] += job_words
 
                     elapsed = time.time() - start
                     if elapsed >= next_report[0]:
-                        logger.info("PROGRESS: at %.2f%% words, alpha %.05f, %.0f words/s" %
-                                    (100.0 * word_count[0] / total_node, alpha, word_count[0] / elapsed if elapsed else 0.0))
-                        next_report[0] = elapsed + 1.0  # don't flood the log, wait at least a second between progress reports
+                        logger.info("PROGRESS: at %.2f%% words\tword_computed %d\talpha %.05f\t %.0f words/s" %
+                                    (100.0 * word_count[0] / total_node, word_count[0], alpha, word_count[0] / elapsed if elapsed else 0.0))
+                        next_report[0] = elapsed + 5.0  # don't flood the log, wait at least a second between progress reports
+                finally:
+                    lock.release()
 
 
 
-        workers = [threading.Thread(target=worker_train) for _ in range(self.workers)]
+        workers = [threading.Thread(target=worker_train, name='thread_'+str(i)) for i in range(self.workers)]
         for thread in workers:
             thread.daemon = True  # make interrupting the process with ctrl+c easier
             thread.start()
 
-        def prepare_sentences():
-            for edge in edges:
-                # avoid calling random_sample() where prob >= 1, to speed things up a little:
-                sampled = [model.vocab[word] for word in edge
-                           if word in model.vocab and (model.vocab[word].sample_probability >= 1.0 or model.vocab[word].sample_probability >= np.random.random_sample())]
-                yield sampled
 
         # convert input strings to Vocab objects (eliding OOV/downsampled words), and start filling the jobs queue
-        for job_no, job in enumerate(chunkize_serial(prepare_sentences(), chunksize)):
+        for job_no, job in enumerate(batch_generator(prepare_sentences(edges), chunksize)):
             # logger.debug("putting job #%i in the queue, qsize=%i" % (job_no, jobs.qsize()))
             jobs.put(job)
-        logger.info("reached the end of input; waiting to finish %i outstanding jobs" % jobs.qsize())
+
+        logger.debug("reached the end of input; waiting to finish %i outstanding jobs" % jobs.qsize())
+        jobs.join()
+        logger.debug("reached the end of input; waiting to finish %i outstanding jobs" % jobs.qsize())
+
         for _ in range(self.workers):
             jobs.put(None)  # give the workers heads up that they can finish -- no more work!
+            logger.debug('put none')
 
         for thread in workers:
             thread.join()
+            logger.debug('thread join')
 
         elapsed = time.time() - start
         logger.warning("training on %i words took %.1fs, %.0f words/s" %
-                    (word_count[0], elapsed, word_count[0] / elapsed if elapsed else 0.0))
+                    (word_count[0], elapsed, word_count[0]/ elapsed if elapsed else 0.0))
---
+++ adsc_pipeline.py
@@ -0,0 +1,128 @@
+__author__ = 'ando'
+import configparser
+import os
+import random
+import sys
+from multiprocessing import cpu_count
+
+
+import numpy as np
+import psutil
+
+from ADSCModel.model import Model
+from ADSCModel.context_embeddings import Context2Vec
+from ADSCModel.node_embeddings import Node2Vec
+from ADSCModel.community_embeddings import Community2Vec
+
+import utils.IO_utils as io_utils
+import utils.graph_utils as graph_utils
+import utils.plot_utils as plot_utils
+import logging
+
+
+
+
+p = psutil.Process(os.getpid())
+try:
+    p.set_cpu_affinity(list(range(cpu_count())))
+except AttributeError:
+    try:
+        p.cpu_affinity(list(range(cpu_count())))
+    except AttributeError:
+        pass
+
+#Setting the logger parameters
+FORMAT = "%(levelname)s %(filename)s: %(lineno)s\t|\t%(message)s"
+level = logging.DEBUG
+logger = logging.getLogger('adsc')
+logger.setLevel(level)
+
+
+prop = configparser.ConfigParser()
+prop.read('conf.ini')
+
+
+
+def process_context(context_learner, model, walks, _lambda1=1.0, _lambda2=0.1, total_nodes=None):
+    logger.info("Training context...")
+    return context_learner.train(model=model, paths=walks, _lambda1=_lambda1, _lambda2=(_lambda2/(model.k * cont_learner.window_size)), total_words=total_nodes)
+
+
+def process_node(node_learner, model, edges, iter=1, lambda2=0.0):
+    logger.info("Training node embedding...")
+    return node_learner.train(model, edges=edges, iter=iter, _lambda2=(lambda2/model.k))
+
+if __name__ == "__main__":
+
+    #Reading the input parameters form the configuration files
+    number_walks = prop.getint('MY', 'number_walks')                      # number of walks for each node
+    walk_length = prop.getint('MY', 'walk_length')                        # length of each walk
+    window_size = prop.getint('MY', 'window_size')                        # windows size used to compute the context embedding
+    negative = prop.getint('MY', 'negative')                              # number of negative sample
+    representation_size = prop.getint('MY', 'representation_size')        # size of the embedding
+    num_workers = prop.getint('MY', 'num_workers')                        # number of thread
+    num_iter = prop.getint('MY', 'num_iter')                              # number of iteration
+    reg_covar = prop.getfloat('MY', 'reg_covar')                          # regularization coefficient to ensure positive covar
+
+    input_file = prop.get('MY', 'input_file_name')                          # name of the input file
+    output_file = prop.get('MY', 'input_file_name')                         # name of the output file
+
+    # lambda_1_val = float(prop.get('MY', 'lambda_1'))                        # alpha parameter for O2
+    # lambda_2_val = float(prop.get('MY', 'lambda_2'))                        # beta parameter for O3
+    # down_sample = float(prop.get('MY', 'down_sample'))
+
+
+    walks_filebase = 'data/' + output_file + ".walks"                       # where read/write the sampled path
+    sampling_path = prop.getboolean('MY', 'sampling_path')                  # execute sampling of new walks
+    pretraining = prop.getboolean('MY', 'pretraining')                      # execute pretraining
+
+
+
+    #CONSTRUCT THE GRAPH
+    G = graph_utils.load_adjacencylist('data/' + input_file + '/' + input_file + '.adjlist', True)
+    # node_color = plot_utils.graph_plot(G=G, save=False, show=False)
+
+    # Sampling the random walks for context
+    walk_files = [walks_filebase + '.' + str(i) for i in range(number_walks)]
+
+
+
+    #Learning algorithm
+    node_learner = Node2Vec(workers=num_workers, negative=negative)
+    cont_learner = Context2Vec(window_size=window_size, workers=num_workers, negative=negative)
+    comm_learner = Community2Vec(reg_covar=reg_covar)
+
+
+    vertex_counts = G.degree(G.nodes_iter())
+
+    model = Model(G=G,
+                  size=representation_size,
+                  min_count=0,
+                  table_size=5000000,
+                  input_file=input_file + '/' + input_file,
+                  vocabulary_counts=vertex_counts,
+                  downsampling=0)
+
+
+    context_total_path = G.number_of_nodes() * number_walks * walk_length
+    logger.info("context_total_node: %d" % (context_total_path))
+    edges = np.array(G.edges())
+
+
+    logger.info('\n_______________________________________\n')
+    model = model.load_model(path='data', file_name=output_file+'_comEmb')
+    for v in model.vocab:
+        model.vocab[v].sample_probability = 1.0
+
+    logger.info('Number of community: %d' % model.k)
+    logger.debug('Number of edges: %d' % len(edges)*2)
+
+    ###########################
+    #   EMBEDDING LEARNING    #
+    ###########################
+    for it in range(1):
+        logging.info('\n_______________________________________\n')
+        comm_learner.train(model)
+        node_learner.train(model, edges=edges, iter=1, _lambda2=(1/model.k), _lambda1=0)
+        io_utils.save_embedding(model.node_embedding, file_name=output_file + "_comEmb_pipeline")
+        model.save(path='data', file_name=output_file + "_comEmb_pipeline")
